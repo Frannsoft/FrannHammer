3db3cb42176ea58dba77ea6aa622342c8250b17a
Jordan Polaniec <frannsoftdev@outlook.com>
2015-10-14 23:03:37 -0400

    - Added core database project.  This will become the lib to communicate with the new db instead of the website.

---
+++ KuroganeHammer.Data.Core/D/Sm4shDB.cs
@@ -0,0 +1,43 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Configuration;
+
+namespace KuroganeHammer.Data.Core.D
+{
+    public class Sm4shDB : IDisposable
+    {
+        private MySqlConnection conn;
+
+        public Sm4shDB()
+        { }
+
+        //TODO: SELECT method
+
+        #region private
+        private MySqlConnection Open()
+        {
+            string cs = ConfigurationManager.AppSettings["connstring"];
+            conn = new MySqlConnection(cs);
+            return conn;
+        }
+
+        private void Close()
+        {
+            if (conn != null)
+            {
+                conn.Close();
+            }
+        }
+        #endregion
+
+        #region IDisposable
+        public void Dispose()
+        {
+            if (conn != null)
+            {
+                conn.Dispose();
+            }
+        }
+        #endregion
+    }
+}
