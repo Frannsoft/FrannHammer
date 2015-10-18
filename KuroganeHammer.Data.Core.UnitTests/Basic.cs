decbecb26cc05f1977e5a055dca0ce5f1878d185
Jordan Polaniec <frannsoftdev@outlook.com>
2015-10-18 00:09:50 -0400

    - Added unit tests project to solution.

---
+++ KuroganeHammer.Data.Core.UnitTests/Basic.cs
@@ -0,0 +1,15 @@
+﻿using KuroganeHammer.Data.Core.Model.Characters;
+using NUnit.Framework;
+
+namespace KuroganeHammer.Data.Core.UnitTests
+{
+    [TestFixture]
+    public class Basic
+    {
+        [Test]
+        public void TestMethod1()
+        {
+            CaptainFalcon falcon = new CaptainFalcon();
+        }
+    }
+}
