ea2281ff0d1f7b451a644efd7e6576ba0f1e81b8
Jordan Polaniec <frannsoftdev@outlook.com>
2015-11-10 19:42:53 -0500

    - Refactored UnitTests to actually be useful - Deleted unnecessary Controllers and DTOs - Fixed some duplicate routing issues

---
+++ KurograneTransferDBTool/KurograneTransferDBTool/BaseTest.cs
@@ -0,0 +1,24 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KurograneTransferDBTool
+{
+    public class BaseTest
+    {
+        protected HttpClient client;
+        protected const string BASEURL = "http://localhost:53410/api/";
+
+        [SetUp]
+        public void SetUp()
+        {
+            client = new HttpClient();
+        }
+
+
+    }
+}
---
+++ KurograneTransferDBTool/KurograneTransferDBTool/Smoke.cs
@@ -0,0 +1,64 @@
+﻿using KuroganeHammer.Data.Core.Model.Characters;
+using NUnit.Framework;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Linq;
+using KuroganeHammer.Data.Core.Model.Stats;
+using System.Net;
+using System;
+using Kurogane.Data.RestApi.DTOs;
+using Newtonsoft.Json;
+
+namespace KurograneTransferDBTool
+{
+    public class Smoke : BaseTest
+    {
+
+
+        [Test]
+        public async Task GetCharacter()
+        {
+            var getResult = await client.GetAsync(BASEURL + "characters/" + 1);
+            string content = await getResult.Content.ReadAsStringAsync();
+
+            Assert.AreEqual(HttpStatusCode.OK, getResult.StatusCode);
+        }
+
+        [Test]
+        public async Task GetMoves()
+        {
+            var getResult = await client.GetAsync(BASEURL + "moves");
+            string content = await getResult.Content.ReadAsStringAsync();
+
+            Assert.AreEqual(HttpStatusCode.OK, getResult.StatusCode);
+        }
+
+        [Test]
+        public async Task GetMoveById()
+        {
+            var getResult = await client.GetAsync(BASEURL + "moves/" + 20);
+            string content = await getResult.Content.ReadAsStringAsync();
+
+            Assert.AreEqual(HttpStatusCode.OK, getResult.StatusCode);
+        }
+
+        [Test]
+        public async Task GetMovesOfType()
+        {
+            var getResult = await client.GetAsync(BASEURL + "movesoftype/" + MoveType.Ground);
+            string content = await getResult.Content.ReadAsStringAsync();
+
+            Assert.AreEqual(HttpStatusCode.OK, getResult.StatusCode);
+        }
+
+        [Test]
+        public void GetCharacterFromKurogane()
+        {
+            var chara = Character.FromId(7);
+        }
+
+
+      
+       
+    }
+}
