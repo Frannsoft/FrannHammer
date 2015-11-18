f9013f93ede2841fcbab073a0d99a022c58fa601
Jordan Polaniec <frannsoftdev@outlook.com>
2015-11-17 22:03:25 -0500

    - Made test project work again

--- Kurogane.Data.RestApi/App_Start/WebApiConfig.cs
+++ Kurogane.Data.RestApi/App_Start/WebApiConfig.cs
@@ -1,10 +1,8 @@
 ﻿using Newtonsoft.Json.Serialization;
 using System.Linq;
-using System.Net.Http;
 using System.Net.Http.Formatting;
 using System.Net.Http.Headers;
 using System.Web.Http;
-using System.Web.Routing;
 
 namespace Kurogane.Data.RestApi
 {
@@ -12,7 +10,6 @@ namespace Kurogane.Data.RestApi
     {
         public static void Register(HttpConfiguration config)
         {
-            // Web API configuration and services
 
             // Web API routes
             config.MapHttpAttributeRoutes();
@@ -22,11 +19,6 @@ namespace Kurogane.Data.RestApi
                 routeTemplate: "api/{controller}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
-
-            //config.Routes.MapHttpRoute(
-            //    name: "DefaultApiWithAction",
-            //    routeTemplate: "api/{controller}/{id}/{action}",
-            //    defaults: new { id = RouteParameter.Optional, action = RouteParameter.Optional });
 
             config.Formatters.JsonFormatter.SupportedMediaTypes.Add(new MediaTypeHeaderValue("text/html"));
             var jsonFormatter = config.Formatters.OfType<JsonMediaTypeFormatter>().First();
