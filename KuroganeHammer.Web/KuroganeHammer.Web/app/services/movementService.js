f3f8209d473543b3f20a98a3c2097b9cf5a334a0
Frannsoft <frannsoftdev@outlook.com>
2016-01-20 22:57:55 -0500

    - Added default api hook for the movementService.

--- KuroganeHammer.Web/KuroganeHammer.Web/app/services/movementService.js
+++ KuroganeHammer.Web/KuroganeHammer.Web/app/services/movementService.js
@@ -1,5 +1,15 @@
 ﻿app.factory('movementService', function ($resource, $rootScope) {
 
+    var requestUri = $rootScope.APIROUTE + 'movement/:id';
+
+    return $resource(requestUri,
+        {
+            id: '@id'
+        });
+});
+
+app.factory('movementService', function ($resource, $rootScope) {
+
     return $resource($rootScope.APIROUTE + 'movement/:id', null,
         {
             'update': { method: 'PUT' }
