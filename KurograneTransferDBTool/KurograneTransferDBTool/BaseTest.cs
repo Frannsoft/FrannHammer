53053b0bf663cf4aa5fc9d6307dfed137940a0db
Frannsoft <frannsoftdev@outlook.com>
2016-02-15 23:28:53 -0500

    - Added support for auth in base test. - Changed model types to use newly moved over (changed namespace) models in core restapi class - This proves the api works!

--- KurograneTransferDBTool/KurograneTransferDBTool/BaseTest.cs
+++ KurograneTransferDBTool/KurograneTransferDBTool/BaseTest.cs
@@ -1,10 +1,10 @@
-﻿using NUnit.Framework;
+﻿using Newtonsoft.Json;
+using NUnit.Framework;
 using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Net.Http;
-using System.Text;
-using System.Threading.Tasks;
+using System.Net.Http.Headers;
+using System.Web;
 
 namespace KurograneTransferDBTool
 {
@@ -12,13 +12,25 @@ namespace KurograneTransferDBTool
     {
         protected HttpClient client;
         protected const string BASEURL = "http://localhost/frannhammerAPI/";
+        string authToken;
 
         [SetUp]
         public void SetUp()
         {
             client = new HttpClient();
+            client.BaseAddress = new Uri("http://localhost/frannHammerAPI");
+            var content = new FormUrlEncodedContent(new[]
+            {
+                new KeyValuePair<string, string>("grant_type", "password"),
+                new KeyValuePair<string, string>("username", "KuroUser"),
+                new KeyValuePair<string, string>("password", "")
+            });
+
+            var response = client.PostAsync("/oauth/token", content).Result;
+            var jsonResponse = response.Content.ReadAsStringAsync().Result;
+            authToken = JsonConvert.DeserializeObject<dynamic>(response.Content.ReadAsStringAsync().Result).access_token;
+
+            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", authToken);
         }
-
-
     }
 }
