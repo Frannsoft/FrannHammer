f0d2d868317874b63259126f6167400953d19c34
Frannsoft <frannsoftdev@outlook.com>
2016-03-06 10:26:50 -0500

    - actually changing to api now..

--- Kurogane.Data.RestApi/App_Start/WebApiConfig.cs
+++ Kurogane.Data.RestApi/App_Start/WebApiConfig.cs
@@ -16,7 +16,7 @@ namespace Kurogane.Data.RestApi
 
             config.Routes.MapHttpRoute(
                 name: "DefaultApi",
-                routeTemplate: "{controller}/{id}",
+                routeTemplate: "api/{controller}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
 
