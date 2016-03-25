4c6b676217b3a408b6831fd9da143da3d3626236
Frannsoft <frannsoftdev@outlook.com>
2016-03-25 09:56:16 -0400

    - new EF CFM model class

---
+++ Kurogane.Data.RestApi/Configuration/CharacterAttributeConfiguration.cs
@@ -0,0 +1,16 @@
+﻿using System.Data.Entity.ModelConfiguration;
+using Kurogane.Data.RestApi.Models;
+
+namespace Kurogane.Data.RestApi.Configuration
+{
+    public class CharacterAttributeConfiguration : EntityTypeConfiguration<CharacterAttribute>
+    {
+        public CharacterAttributeConfiguration()
+        {
+            ToTable("attributes");
+            Property(a => a.Name).IsRequired();
+            Property(a => a.Value).IsRequired();
+            Property(a => a.AttributeType).IsRequired();
+        }
+    }
+}
