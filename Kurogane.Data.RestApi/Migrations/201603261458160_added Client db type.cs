a406b64fe692325d2b589305768da9f8c19ad537
Frannsoft <frannsoftdev@outlook.com>
2016-03-26 14:07:21 -0400

    - Migrations and csproj updates for classes that were in testing

---
+++ Kurogane.Data.RestApi/Migrations/201603261458160_added Client db type.cs
@@ -0,0 +1,31 @@
+namespace Kurogane.Data.RestApi.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class addedClientdbtype : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Clients",
+                c => new
+                    {
+                        Id = c.String(nullable: false, maxLength: 128),
+                        Secret = c.String(nullable: false),
+                        Name = c.String(nullable: false, maxLength: 100),
+                        ApplicationType = c.Int(nullable: false),
+                        Active = c.Boolean(nullable: false),
+                        RefreshTokenLifeTime = c.Int(nullable: false),
+                        AllowedOrigin = c.String(maxLength: 100),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Clients");
+        }
+    }
+}
