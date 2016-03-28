2004eb1331ba8a9a134e1a8c68b440460048ab43
Frannsoft <frannsoftdev@outlook.com>
2016-03-28 10:26:40 -0400

    - minor refactoring - Ignored getmovebyId test as it relies on hardcoded ID to pass.  Shouldn't do this.

--- KuroganeHammer.Web/KuroganeHammer.Web/app/domain/character.ts
+++ KuroganeHammer.Web/KuroganeHammer.Web/app/domain/character.ts
@@ -1,6 +1,5 @@
 ﻿module app.domain {
     export interface ICharacter extends ng.resource.IResource<ICharacter>{
-        
         metaData: domain.ICharacterMetadata;
         movementData: domain.IMovement[];
         moveData: domain.IMove[];
