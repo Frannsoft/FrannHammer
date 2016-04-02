e4b903b15b734097ebbce509655a756c18074a94
Frannsoft <frannsoftdev@outlook.com>
2016-04-02 18:19:36 -0400

    - Added test classes and basic tests for all controllers - Added BaseTestController which contains methods to spin up model objects and controller instances

--- KuroganeHammer.Data/KuroganeHammer.Data.Api.Tests/Controllers/CharacterControllerTest.cs
+++ KuroganeHammer.Data/KuroganeHammer.Data.Api.Tests/Controllers/CharacterControllerTest.cs
@@ -1,117 +1,32 @@
 ﻿using System;
-using System.Data.Common;
 using System.Linq;
 using System.Web.Http.Results;
-using Effort;
-using KuroganeHammer.Data.Api.Controllers;
 using NUnit.Framework;
 using KuroganeHammer.Data.Api.Models;
 
 namespace KuroganeHammer.Data.Api.Tests.Controllers
 {
     [TestFixture]
-    public class CharacterControllerTest
+    public class CharacterControllerTest : BaseControllerTest
     {
-        //private IQueryable<Character> _characters;
-        //private IQueryable<Movement> _movements;
-        //private IQueryable<Move> _moves;
-        //private Mock<ApplicationDbContext> _contextMock;
-
-        private DbConnection _connection;
-        private ApplicationDbContext _dbContext;
-        private CharactersController _charactersController;
-        private MovesController _movesController;
-        private MovementsController _movementsController;
-
-        [SetUp]
-        public void Setup()
+        [Test]
+        public void ShouldGetCharacter()
         {
-            _connection = DbConnectionFactory.CreateTransient();
-            _dbContext = new ApplicationDbContext(_connection);
-            _charactersController = new CharactersController(_dbContext);
-            _movesController = new MovesController(_dbContext);
-            _movementsController = new MovementsController(_dbContext);
+            var character = TestObjects.Character();
+            CharactersController.PostCharacter(character);
 
-            //_characters = new List<Character>
-            //{
-            //    new Character
-            //    {
-            //        ColorTheme = "#323423",
-            //        Description = "desc",
-            //        Id = 1,
-            //        LastModified = DateTime.Now,
-            //        MainImageUrl = "http://img.com/i.png",
-            //        Name = "falco",
-            //        Style = "prefers the air",
-            //        ThumbnailUrl = "http://img.net/ii.png"
-            //    }
-            //}.AsQueryable();
+            var result = CharactersController.GetCharacter(character.Id) as OkNegotiatedContentResult<Character>;
 
-            //_movements = new List<Movement>
-            //{
-            //    new Movement
-            //    {
-            //        Id = 1,
-            //        LastModified = DateTime.Now,
-            //        Name = "jab 1",
-            //        OwnerId = 1,
-            //        Value = "3 frames"
-            //    }
-            //}.AsQueryable();
-
-            //_moves = new List<Move>
-            //{
-            //    new Move
-            //    {
-            //        Angle = "40",
-            //        AutoCancel = "3",
-            //        BaseDamage = "53",
-            //        BaseKnockBackSetKnockback = "60/25",
-            //        FirstActionableFrame = 2,
-            //        HitboxActive = "23-25",
-            //        Id = 1,
-            //        KnockbackGrowth = "89",
-            //        LandingLag = "11",
-            //        LastModified = DateTime.Now,
-            //        Name = "falco phantasm",
-            //        OwnerId = 1,
-            //        TotalHitboxActiveLength = 30,
-            //        Type = MoveType.Special
-            //    }
-            //}.AsQueryable();
-
-            //_contextMock = new Mock<ApplicationDbContext>();
+            Assert.That(result?.Content, Is.Not.Null);
         }
-
-        //private Mock<IDbSet<T>> SetupMockDbSet<T>(IQueryable<T> queryableToMock, Func<object[], T> findFunc)
-        //    where T : class
-        //{
-        //    var mockSet = new Mock<IDbSet<T>>();
-        //    mockSet.Setup(m => m.Provider).Returns(queryableToMock.Provider);
-        //    mockSet.Setup(m => m.Expression).Returns(queryableToMock.Expression);
-        //    mockSet.Setup(m => m.ElementType).Returns(queryableToMock.ElementType);
-        //    mockSet.Setup(m => m.GetEnumerator()).Returns(queryableToMock.GetEnumerator());
-        //    mockSet.Setup(m => m.Find(It.IsAny<object[]>())).Returns(findFunc);
-
-        //    return mockSet;
-        //}
 
         [Test]
         public void ShouldGetAllCharacters()
         {
-            var newCharacter = new Character
-            {
-                ColorTheme = "#32523",
-                Description = "desc",
-                Id = 0,
-                LastModified = DateTime.Now,
-                Name = "mewtwo",
-                Style = "phaser"
-            };
+            var newCharacter = TestObjects.Character();
+            CharactersController.PostCharacter(newCharacter);
 
-            _charactersController.PostCharacter(newCharacter);
-
-            var results = _charactersController.GetCharacters();
+            var results = CharactersController.GetCharacters();
 
             Assert.That(results.Count(), Is.EqualTo(1));
         }
@@ -119,30 +34,13 @@ namespace KuroganeHammer.Data.Api.Tests.Controllers
         [Test]
         public void ShouldGetAllMovementsForCharacter()
         {
-            var newCharacter = new Character
-            {
-                ColorTheme = "#32523",
-                Description = "desc",
-                Id = 0,
-                LastModified = DateTime.Now,
-                Name = "mewtwo",
-                Style = "phaser"
-            };
+            var newCharacter = TestObjects.Character();
+            CharactersController.PostCharacter(newCharacter);
 
-            _charactersController.PostCharacter(newCharacter);
+            var movement = TestObjects.Movement();
+            MovementsController.PostMovement(movement);
 
-            var movement = new Movement
-            {
-                Id = 1,
-                LastModified = DateTime.Now,
-                Name = "jab 1",
-                OwnerId = 0,
-                Value = "3 frames"
-            };
-
-            _movementsController.PostMovement(movement);
-
-            var results = _charactersController.GetMovementsForCharacter(0);
+            var results = CharactersController.GetMovementsForCharacter(newCharacter.Id);
 
             Assert.That(results.Count(), Is.EqualTo(1));
             Assert.That(results.First().Name, Is.EqualTo("jab 1"));
@@ -151,39 +49,14 @@ namespace KuroganeHammer.Data.Api.Tests.Controllers
         [Test]
         public void ShouldGetAllMovesForCharacter()
         {
-            var newCharacter = new Character
-            {
-                ColorTheme = "#32523",
-                Description = "desc",
-                Id = 0,
-                LastModified = DateTime.Now,
-                Name = "mewtwo",
-                Style = "phaser"
-            };
+            var newCharacter = TestObjects.Character();
 
-            var move = new Move
-            {
-                Angle = "40",
-                AutoCancel = "3",
-                BaseDamage = "53",
-                BaseKnockBackSetKnockback = "60/25",
-                FirstActionableFrame = 2,
-                HitboxActive = "23-25",
-                Id = 1,
-                KnockbackGrowth = "89",
-                LandingLag = "11",
-                LastModified = DateTime.Now,
-                Name = "falco phantasm",
-                OwnerId = 0,
-                TotalHitboxActiveLength = 30,
-                Type = MoveType.Special
-            };
+            var move = TestObjects.Move();
+            MovesController.PostMove(move);
 
-            _movesController.PostMove(move);
+            CharactersController.PostCharacter(newCharacter);
 
-            _charactersController.PostCharacter(newCharacter);
-
-            var results = _charactersController.GetMovesForCharacter(0);
+            var results = CharactersController.GetMovesForCharacter(newCharacter.Id);
 
             Assert.That(results.Count(), Is.EqualTo(1));
             Assert.That(results.First().Name, Is.EqualTo("falco phantasm"));
@@ -192,69 +65,49 @@ namespace KuroganeHammer.Data.Api.Tests.Controllers
         [Test]
         public void ShouldAddCharacter()
         {
-            var newCharacter = new Character
-            {
-                ColorTheme = "#32523",
-                Description = "desc",
-                Id = 0,
-                LastModified = DateTime.Now,
-                Name = "mewtwo",
-                Style = "phaser"
-            };
-            var result = _charactersController.PostCharacter(newCharacter) as CreatedAtRouteNegotiatedContentResult<Character>;
+            var newCharacter = TestObjects.Character();
+            var result = CharactersController.PostCharacter(newCharacter) as CreatedAtRouteNegotiatedContentResult<Character>;
 
             Assert.That(result, Is.Not.Null);
-            Assert.That(result.Content, Is.Not.Null);
-            Assert.That(result.Content.Name, Is.EqualTo("mewtwo"));
+            Assert.That(result?.Content, Is.Not.Null);
+            Assert.AreEqual(newCharacter, result?.Content);
         }
 
         [Test]
         public void ShouldUpdateCharacter()
         {
-            var character = new Character
-            {
+            const string expectedName = "mewtwo";
+            var character = TestObjects.Character();
 
-                ColorTheme = "#32523",
-                Description = "desc",
-                Id = 0,
-                LastModified = DateTime.Now,
-                Name = "mewtwo",
-                Style = "phaser"
-            };
+            var dateTime = DateTime.Now;
 
             //arrange
-            _charactersController.PostCharacter(character);
-
+            var returnedCharacter =
+                CharactersController.PostCharacter(character) as CreatedAtRouteNegotiatedContentResult<Character>;
             //act
-            _charactersController.PutCharacter(0, new Character
+            if (returnedCharacter != null)
             {
-                Name = "falco prefers the air",
-                Id = 0
-            });
+                returnedCharacter.Content.Name = expectedName;
+                CharactersController.PutCharacter(returnedCharacter.Content.Id, returnedCharacter.Content);
+            }
 
-            var updatedCharacter = _charactersController.GetCharacter(0) as OkNegotiatedContentResult<Character>;
+            var updatedCharacter = CharactersController.GetCharacter(character.Id) as OkNegotiatedContentResult<Character>;
 
             //assert
-            Assert.That(updatedCharacter.Content.Name, Is.EqualTo("falco prefers the air"));
+            Assert.That(updatedCharacter?.Content.Name, Is.EqualTo(expectedName));
+            Assert.That(updatedCharacter?.Content.LastModified, Is.GreaterThan(dateTime));
         }
 
         [Test]
-        public void Effort_ShouldAddCharacter()
+        public void ShouldDeleteCharacter()
         {
-            var character = new Character
-            {
+            var character = TestObjects.Character();
+            CharactersController.PostCharacter(character);
 
-                ColorTheme = "#32523",
-                Description = "desc",
-                Id = 0,
-                LastModified = DateTime.Now,
-                Name = "mewtwo",
-                Style = "phaser"
-            };
+            CharactersController.DeleteCharacter(character.Id);
 
-            var result = _charactersController.PostCharacter(character) as CreatedAtRouteNegotiatedContentResult<Character>;
-
-            Assert.That(result.Content.Name, Is.EqualTo("mewtwo"));
+            var result = CharactersController.GetCharacter(character.Id) as NotFoundResult;
+            Assert.That(result, Is.Not.Null);
         }
     }
 }
---
+++ KuroganeHammer.Data/KuroganeHammer.Data.Api.Tests/Controllers/MovementsControllerTest.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Linq;
+using System.Web.Http.Results;
+using KuroganeHammer.Data.Api.Models;
+using NUnit.Framework;
+
+namespace KuroganeHammer.Data.Api.Tests.Controllers
+{
+    [TestFixture]
+    public class MovementsControllerTest : BaseControllerTest
+    {
+
+        [Test]
+        public void ShouldGetMovements()
+        {
+            var movement = TestObjects.Movement();
+            MovementsController.PostMovement(movement);
+
+            var result = MovementsController.GetMovement(movement.Id) as OkNegotiatedContentResult<Movement>;
+
+            Assert.That(result, Is.Not.Null);
+        }
+
+        [Test]
+        public void ShouldGetAllMovements()
+        {
+            var movements = TestObjects.Movement();
+            MovementsController.PostMovement(movements);
+
+            var results = MovementsController.GetMovements();
+
+            Assert.That(results.Count(), Is.EqualTo(1));
+        }
+
+        [Test]
+        public void ShouldAddMovements()
+        {
+            var movement = TestObjects.Movement();
+            var result = MovementsController.PostMovement(movement) as CreatedAtRouteNegotiatedContentResult<Movement>;
+
+            Assert.That(result, Is.Not.Null);
+            Assert.That(result?.Content, Is.Not.Null);
+            Assert.AreEqual(movement, result?.Content);
+        }
+
+        [Test]
+        public void ShouldUpdateMovements()
+        {
+            const string expectedName = "jab 2";
+            var movement = TestObjects.Movement();
+
+            var dateTime = DateTime.Now;
+
+            var returnedMovements =
+                MovementsController.PostMovement(movement) as CreatedAtRouteNegotiatedContentResult<Movement>;
+
+            if (returnedMovements != null)
+            {
+                returnedMovements.Content.Name = expectedName;
+                MovementsController.PutMovement(returnedMovements.Content.Id, returnedMovements.Content);
+            }
+
+            var updatedMovements = MovementsController.GetMovement(movement.Id) as OkNegotiatedContentResult<Movement>;
+
+            Assert.That(updatedMovements?.Content.Name, Is.EqualTo(expectedName));
+            Assert.That(updatedMovements?.Content.LastModified, Is.GreaterThan(dateTime));
+        }
+
+        [Test]
+        public void ShouldDeleteMovements()
+        {
+            var movement = TestObjects.Movement();
+            MovementsController.PostMovement(movement);
+
+            MovementsController.DeleteMovement(movement.Id);
+
+            var result = MovementsController.GetMovement(movement.Id) as NotFoundResult;
+            Assert.That(result, Is.Not.Null);
+        }
+    }
+}
