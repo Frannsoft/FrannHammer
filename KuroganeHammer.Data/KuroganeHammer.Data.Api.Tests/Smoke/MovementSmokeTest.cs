4e2e615812facb224ea9db10422d86ea4d23f09b
Frannsoft <frannsoftdev@outlook.com>
2016-04-03 09:49:15 -0400

    - Added integration tests that smoke test get calls among a few other calls.  These are full integration tests unlike the other controller tests in this project which are much more lightweight.

---
+++ KuroganeHammer.Data/KuroganeHammer.Data.Api.Tests/Smoke/MovementSmokeTest.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using KuroganeHammer.Data.Api.Models;
+using NUnit.Framework;
+
+namespace KuroganeHammer.Data.Api.Tests.Smoke
+{
+    [TestFixture]
+    public class MovementSmokeTest : BaseSmokeTest
+    {
+        private List<Movement> _movements;
+
+        [SetUp]
+        public override void SetUp()
+        {
+            base.SetUp();
+            _movements = LoggedInBasicClient.GetAsync(Baseuri + MovementsRoute).Result.Content.ReadAsAsync<List<Movement>>().Result;
+        }
+
+        [Test]
+        public void ShouldGetAllMovements()
+        {
+            CollectionAssert.AllItemsAreNotNull(_movements);
+            CollectionAssert.AllItemsAreUnique(_movements);
+        }
+
+        [Test]
+        public async Task ShouldGetMovement()
+        {
+            var result = await LoggedInBasicClient.GetAsync(Baseuri + MovementsRoute + "/" + _movements[0].Id);
+
+            Assert.IsTrue(result.IsSuccessStatusCode);
+
+            var movement = result.Content.ReadAsAsync<Movement>().Result;
+            Assert.That(movement != null);
+            Assert.That(movement.Name.Length > 0);
+        }
+
+        [Test]
+        public async Task ShouldNotGetAllMovementsDueToNoAuth()
+        {
+            var result = await AnonymousClient.GetAsync(Baseuri + MovementsRoute);
+            Assert.That(result.StatusCode, Is.EqualTo(HttpStatusCode.Unauthorized));
+        }
+    }
+}
