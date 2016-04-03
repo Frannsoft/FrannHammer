7bf546273509c9f86f1015e83126bf53a88ed972
Frannsoft <frannsoftdev@outlook.com>
2016-04-03 09:50:22 -0400

    - Created rolesconstants file to help with roles accuracy

---
+++ KuroganeHammer.Data/KuroganeHammer.Data.Api/Models/RolesConstants.cs
@@ -0,0 +1,8 @@
+﻿namespace KuroganeHammer.Data.Api.Models
+{
+    public class RolesConstants
+    {
+        public const string Basic = "Basic";
+        public const string Admin = "Admin";
+    }
+}
