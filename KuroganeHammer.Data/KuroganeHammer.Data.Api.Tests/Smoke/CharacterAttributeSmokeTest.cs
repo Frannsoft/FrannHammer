946f146eb96976bed1f08f4a9902206068e206e4
Frannsoft <frannsoftdev@outlook.com>
2016-04-03 16:37:58 -0400

    - Added test to get all characterattributes of a specific smashattributetype

--- KuroganeHammer.Data/KuroganeHammer.Data.Api.Tests/Smoke/CharacterAttributeSmokeTest.cs
+++ KuroganeHammer.Data/KuroganeHammer.Data.Api.Tests/Smoke/CharacterAttributeSmokeTest.cs
@@ -2,7 +2,10 @@
 using System.Net;
 using System.Net.Http;
 using System.Threading.Tasks;
+using KuroganeHammer.Data.Api.DTOs;
 using KuroganeHammer.Data.Api.Models;
+using KuroganeHammer.Data.Api.Tests.Asserts;
+using Newtonsoft.Json;
 using NUnit.Framework;
 
 namespace KuroganeHammer.Data.Api.Tests.Smoke
@@ -24,10 +27,11 @@ namespace KuroganeHammer.Data.Api.Tests.Smoke
         {
             CollectionAssert.AllItemsAreNotNull(_attributes);
             CollectionAssert.AllItemsAreUnique(_attributes);
+            CollectionPropertyAssert.AllPropertiesNotNullInCollection(_attributes, "SmashAttributeType");
         }
 
         [Test]
-        public async Task ShouldGetSmashAttributeType()
+        public async Task ShouldGetCharacterAttribute()
         {
             var result = await LoggedInBasicClient.GetAsync(Baseuri + CharacterAttributeRoute + "/" + _attributes[0].Id);
 
@@ -39,7 +43,30 @@ namespace KuroganeHammer.Data.Api.Tests.Smoke
         }
 
         [Test]
-        public async Task ShouldNotGetAllSmashAttributeTypesDueToNoAuth()
+        public async Task ShouldGetAllCharacterAttributesofSmashAttributeType()
+        {
+            var attributeTypes =
+                LoggedInBasicClient.GetAsync(Baseuri + SmashAttributeTypeRoute)
+                    .Result.Content.ReadAsAsync<List<SmashAttributeType>>()
+                    .Result;
+
+            var result =
+                await
+                    LoggedInBasicClient.GetAsync(Baseuri + SmashAttributeTypeRoute + "/" + attributeTypes[3].Id + "/" +
+                                                 "characterattributes");
+
+            Assert.IsTrue(result.IsSuccessStatusCode);
+
+            var json = await result.Content.ReadAsStringAsync();
+            var characterAttributeRows = JsonConvert.DeserializeObject<List<CharacterAttributeRowDto>>(json);
+            //var characterAttributeRows = await result.Content.ReadAsAsync<List<CharacterAttributeRowDto>>();
+
+            CollectionAssert.AllItemsAreNotNull(characterAttributeRows);
+            CollectionAssert.AllItemsAreUnique(characterAttributeRows);
+        }
+
+        [Test]
+        public async Task ShouldNotGetAllCharacterAttributesDueToNoAuth()
         {
             var result = await AnonymousClient.GetAsync(Baseuri + CharacterAttributeRoute);
             Assert.That(result.StatusCode, Is.EqualTo(HttpStatusCode.Unauthorized));
