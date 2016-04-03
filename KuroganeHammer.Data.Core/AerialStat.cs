589c5e444874fa195c29c3cd2a898ebd32eb08d9
Frannsoft <frannsoftdev@outlook.com>
2016-04-03 11:47:09 -0400

    - Added Stat types necessary for scraping to the Data.Core project.

---
+++ KuroganeHammer.Data.Core/AerialStat.cs
@@ -0,0 +1,15 @@
+﻿
+namespace KuroganeHammer.Data.Core
+{
+    public class AerialStat : MoveStat
+    {
+        public AerialStat(string name, int ownerId, string hitboxActive, string firstActionableFrame, string baseDamage,
+            string angle, string baseKnockbackSetKnockback, string knockbackGrowth, string landingLag, string autoCancel)
+            : base(MoveType.Aerial, name, ownerId, hitboxActive, firstActionableFrame, baseDamage, angle, baseKnockbackSetKnockback,
+                    knockbackGrowth, landingLag, autoCancel)
+        { }
+
+        public AerialStat()
+        { }
+    }
+}
---
+++ KuroganeHammer.Data.Core/CharacterStat.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace KuroganeHammer.Data.Core
+{
+    public class CharacterStat
+    {
+        public string Style { get; set; }
+        public string MainImageUrl { get; set; }
+        public string ThumbnailUrl { get; set; }
+        public string Description { get; set; }
+        public string ColorTheme { get; set; }
+        public string Name { get; set; }
+        public int Id { get; set; }
+        public DateTime LastModified { get; set; }
+
+        public CharacterStat(int id, string name, string style, string mainImageUrl,
+            string thumbnailUrl, string colorTheme, string description, DateTime lastModified)
+        {
+            Name = name;
+            Id = id;
+            Style = style;
+            MainImageUrl = mainImageUrl;
+            ThumbnailUrl = thumbnailUrl;
+            Description = description;
+            ColorTheme = colorTheme;
+            LastModified = lastModified;
+        }
+
+        public CharacterStat()
+        { }
+
+    }
+}
