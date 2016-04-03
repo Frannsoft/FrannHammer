5b08cdaf03b79894a683dbc291f8b2c44fde8129
Frannsoft <frannsoftdev@outlook.com>
2016-04-03 16:37:37 -0400

    - Created model DTOs

---
+++ KuroganeHammer.Data/KuroganeHammer.Data.Api/DTOs/CharacterAttributeDto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace KuroganeHammer.Data.Api.DTOs
+{
+    public class CharacterAttributeDto
+    {
+        public int OwnerId { get; set; }
+        public string Rank { get; set; }
+        public string Value { get; set; }
+        public string Name { get; set; }
+        public int Id { get; set; }
+        public SmashAttributeTypeDto SmashAttributeTypeDto { get; set; }
+        public int SmashAttributeTypeId { get; set; }
+    }
+
+    
+}
