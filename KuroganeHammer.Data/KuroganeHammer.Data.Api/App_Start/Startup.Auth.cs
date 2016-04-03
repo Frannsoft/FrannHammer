03d10b2e626dd34a14a2f50a17e929bd1cf1b2f2
Frannsoft <frannsoftdev@outlook.com>
2016-04-03 09:47:14 -0400

    - Adjusted Token call for authentatication to be api/Token which is more in-line with the rest of the api

--- KuroganeHammer.Data/KuroganeHammer.Data.Api/App_Start/Startup.Auth.cs
+++ KuroganeHammer.Data/KuroganeHammer.Data.Api/App_Start/Startup.Auth.cs
@@ -31,7 +31,7 @@ namespace KuroganeHammer.Data.Api
             PublicClientId = "self";
             OAuthOptions = new OAuthAuthorizationServerOptions
             {
-                TokenEndpointPath = new PathString("/Token"),
+                TokenEndpointPath = new PathString("/api/Token"),
                 Provider = new ApplicationOAuthProvider(PublicClientId),
                 AuthorizeEndpointPath = new PathString("/api/Account/ExternalLogin"),
                 AccessTokenExpireTimeSpan = TimeSpan.FromDays(14),
