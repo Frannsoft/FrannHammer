f7f3fde6529a8dbd0c879834d215e148a7fd726f
Frannsoft <frannsoftdev@outlook.com>
2016-04-03 09:49:15 -0400

    - Added integration tests that smoke test get calls among a few other calls.  These are full integration tests unlike the other controller tests in this project which are much more lightweight.

---
+++ KuroganeHammer.Data/KuroganeHammer.Data.Api.Tests/Smoke/CharacterAttributeSmokeTest.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using KuroganeHammer.Data.Api.Models;
+using NUnit.Framework;
+
+namespace KuroganeHammer.Data.Api.Tests.Smoke
+{
+    [TestFixture]
+    public class CharacterAttributeSmokeTest : BaseSmokeTest
+    {
+        private List<CharacterAttribute> _attributes;
+
+        [SetUp]
+        public override void SetUp()
+        {
+            base.SetUp();
+            _attributes = LoggedInBasicClient.GetAsync(Baseuri + CharacterAttributeRoute).Result.Content.ReadAsAsync<List<CharacterAttribute>>().Result;
+        }
+
+        [Test]
+        public void ShouldGetAllSmashAttributeTypes()
+        {
+            CollectionAssert.AllItemsAreNotNull(_attributes);
+            CollectionAssert.AllItemsAreUnique(_attributes);
+        }
+
+        [Test]
+        public async Task ShouldGetSmashAttributeType()
+        {
+            var result = await LoggedInBasicClient.GetAsync(Baseuri + CharacterAttributeRoute + "/" + _attributes[0].Id);
+
+            Assert.IsTrue(result.IsSuccessStatusCode);
+
+            var attributeType = result.Content.ReadAsAsync<CharacterAttribute>().Result;
+            Assert.That(attributeType != null);
+            Assert.That(attributeType.Name.Length > 0);
+        }
+
+        [Test]
+        public async Task ShouldNotGetAllSmashAttributeTypesDueToNoAuth()
+        {
+            var result = await AnonymousClient.GetAsync(Baseuri + CharacterAttributeRoute);
+            Assert.That(result.StatusCode, Is.EqualTo(HttpStatusCode.Unauthorized));
+        }
+    }
+}
---
+++ KuroganeHammer.Data/KuroganeHammer.Data.Api.Tests/Smoke/MoveSmokeTest.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+using System.Net;
+using System.Threading.Tasks;
+using KuroganeHammer.Data.Api.Models;
+using NUnit.Framework;
+using System.Net.Http;
+
+namespace KuroganeHammer.Data.Api.Tests.Smoke
+{
+    [TestFixture]
+    public class MoveSmokeTest : BaseSmokeTest
+    {
+        private List<Move> _moves;
+
+        [SetUp]
+        public override void SetUp()
+        {
+            base.SetUp();
+            _moves = LoggedInBasicClient.GetAsync(Baseuri + MovesRoute).Result.Content.ReadAsAsync<List<Move>>().Result;
+        }
+
+        [Test]
+        public void ShouldGetAllMoves()
+        {
+            CollectionAssert.AllItemsAreNotNull(_moves);
+            CollectionAssert.AllItemsAreUnique(_moves);
+        }
+
+        [Test]
+        public async Task ShouldGetMove()
+        {
+            var result = await LoggedInBasicClient.GetAsync(Baseuri + MovesRoute + "/" + _moves[0].Id);
+            Assert.IsTrue(result.IsSuccessStatusCode);
+
+            var move = result.Content.ReadAsAsync<Move>().Result;
+            Assert.That(move != null);
+            Assert.That(move.Name.Length > 0);
+        }
+
+        [Test]
+        public async Task ShouldNotGetAllMovesDueToNoAuth()
+        {
+            var result = await AnonymousClient.GetAsync(Baseuri + MovesRoute);
+            Assert.That(result.StatusCode, Is.EqualTo(HttpStatusCode.Unauthorized));
+        }
+    }
+}
