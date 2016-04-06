67b67da0253113d2a338438a88deeb87bcb49bec
Frannsoft <frannsoftdev@outlook.com>
2016-04-05 23:18:19 -0400

    - Added WebApiThrottle nuget package.  This enables request logging and throttling based on passed in settings.  This is now live in the API, but only in Release mode.

--- KuroganeHammer.Data/KuroganeHammer.Data.Api/App_Start/WebApiConfig.cs
+++ KuroganeHammer.Data/KuroganeHammer.Data.Api/App_Start/WebApiConfig.cs
@@ -1,14 +1,21 @@
-﻿using System.Linq;
+﻿using System;
+using System.Configuration;
+using System.Linq;
 using System.Net.Http.Formatting;
 using System.Net.Http.Headers;
 using System.Web.Http;
 using Microsoft.Owin.Security.OAuth;
 using Newtonsoft.Json.Serialization;
+using WebApiThrottle;
 
 namespace KuroganeHammer.Data.Api
 {
     public static class WebApiConfig
     {
+        #region custom config keys
+        private const string KeyThrottleRequestsPerSecond = "throttlingrequestspersecond";
+        #endregion
+
         public static void Register(HttpConfiguration config)
         {
             // Web API configuration and services
@@ -25,11 +32,36 @@ namespace KuroganeHammer.Data.Api
                 defaults: new { id = RouteParameter.Optional }
             );
 
+#if !DEBUG
+            int throttleRequestsPerSecond = GetConfigValue<int>(KeyThrottleRequestsPerSecond);
+
+            config.MessageHandlers.Add(new ThrottlingHandler()
+            {
+                Policy = new ThrottlePolicy(perSecond: throttleRequestsPerSecond)
+                {
+                    IpThrottling = true
+                },
+                Repository = new CacheRepository()
+            });
+#endif
 
             config.Formatters.JsonFormatter.SupportedMediaTypes.Add(new MediaTypeHeaderValue("text/html"));
             var jsonFormatter = config.Formatters.OfType<JsonMediaTypeFormatter>().First();
             jsonFormatter.SerializerSettings.Formatting = Newtonsoft.Json.Formatting.Indented;
             jsonFormatter.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+        }
+
+        private static T GetConfigValue<T>(string key)
+        {
+            T retVal = default(T);
+            var configValue = ConfigurationManager.AppSettings[key];
+
+            if (string.IsNullOrEmpty(configValue))
+            { throw new Exception($"Unable to find key of {key}"); }
+
+            retVal = (T)Convert.ChangeType(configValue, typeof(T));
+
+            return retVal;
         }
     }
 }
