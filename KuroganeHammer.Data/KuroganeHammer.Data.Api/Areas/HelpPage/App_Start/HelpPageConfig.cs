b683ce9d085c942221c12e2d7d6186bc51c377ed
Frannsoft <frannsoftdev@outlook.com>
2016-04-07 23:17:32 -0400

    - Configured xml comments to be on for API - Spruced up API help page a bit

--- KuroganeHammer.Data/KuroganeHammer.Data.Api/Areas/HelpPage/App_Start/HelpPageConfig.cs
+++ KuroganeHammer.Data/KuroganeHammer.Data.Api/Areas/HelpPage/App_Start/HelpPageConfig.cs
@@ -4,6 +4,7 @@
 
 using System.Diagnostics.CodeAnalysis;
 using System.Net.Http.Headers;
+using System.Web;
 using System.Web.Http;
 #if Handle_PageResultOfT
 using System.Web.Http.OData;
@@ -27,7 +28,7 @@ namespace KuroganeHammer.Data.Api.Areas.HelpPage
         public static void Register(HttpConfiguration config)
         {
             //// Uncomment the following to use the documentation from XML documentation file.
-            //config.SetDocumentationProvider(new XmlDocumentationProvider(HttpContext.Current.Server.MapPath("~/App_Data/XmlDocument.xml")));
+            config.SetDocumentationProvider(new XmlDocumentationProvider(HttpContext.Current.Server.MapPath("~/App_Data/XmlDocument.xml")));
 
             //// Uncomment the following to use "sample string" as the sample for all actions that have string as the body parameter or return type.
             //// Also, the string arrays will be used for IEnumerable<string>. The sample objects will be serialized into different media type 
