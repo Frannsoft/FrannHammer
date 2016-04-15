8142a4ed779e9485f5aabceef65889ca2ae07114
Frannsoft <frannsoftdev@outlook.com>
2016-04-14 22:06:04 -0400

    - Now creating a role manager in owincontext

--- KuroganeHammer.Data/KuroganeHammer.Data.Api/App_Start/Startup.Auth.cs
+++ KuroganeHammer.Data/KuroganeHammer.Data.Api/App_Start/Startup.Auth.cs
@@ -21,6 +21,7 @@ namespace KuroganeHammer.Data.Api
             // Configure the db context and user manager to use a single instance per request
             app.CreatePerOwinContext(ApplicationDbContext.Create);
             app.CreatePerOwinContext<ApplicationUserManager>(ApplicationUserManager.Create);
+            app.CreatePerOwinContext<ApplicationRoleManager>(ApplicationRoleManager.Create);
 
             // Enable the application to use a cookie to store information for the signed in user
             // and to use a cookie to temporarily store information about a user logging in with a third party login provider
