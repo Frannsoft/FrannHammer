c2105e42af34d59c4214de067292a969f185ac98
Frannsoft <frannsoftdev@outlook.com>
2016-04-14 22:07:19 -0400

    - Added UserName property to user binding model.  Should fix the confusion between what to use to login as.

--- KuroganeHammer.Data/KuroganeHammer.Data.Api/Models/AccountBindingModels.cs
+++ KuroganeHammer.Data/KuroganeHammer.Data.Api/Models/AccountBindingModels.cs
@@ -37,6 +37,10 @@ namespace KuroganeHammer.Data.Api.Models
         public string Email { get; set; }
 
         [Required]
+        [Display(Name = "UserName")]
+        public string UserName { get; set; }
+
+        [Required]
         [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
         [DataType(DataType.Password)]
         [Display(Name = "Password")]
