772ff4c86cb98d80ec6aa48385a32d762320e5de
Frannsoft <frannsoftdev@outlook.com>
2016-04-14 22:08:36 -0400

    - Updated tests to take advantage of owin.testing and use in-memory hosting - Working on support for in-memory authentication through owin.testing - Added tests for ensuring unauth'd errors are thrown on characters requests when the caller is not logged in / does not pass in an auth token

---
+++ KuroganeHammer.Data/KuroganeHammer.Data.Api.Tests/Controllers/BaseAuthenticatedTest.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.Owin.Testing;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using Newtonsoft.Json.Linq;
+using NUnit.Framework;
+using System.Threading.Tasks;
+
+namespace KuroganeHammer.Data.Api.Tests.Controllers
+{
+    public class BaseAuthenticatedTest : BaseControllerTest
+    {
+        protected virtual string Username => "GETuser";
+        protected virtual string Password => "GETpassword";
+
+        private string _token;
+
+        protected override void PostSetup(TestServer server)
+        {
+            var tokenDetails = new FormUrlEncodedContent(new[]
+            {
+                new KeyValuePair<string, string>("grant_type", "password"),
+                new KeyValuePair<string, string>("username", Username),
+                new KeyValuePair<string, string>("password", Password)
+            });
+
+            var tokenResult = server.HttpClient.PostAsync("/api/token", tokenDetails).Result;
+            Assert.That(tokenResult.StatusCode, Is.EqualTo(HttpStatusCode.OK));
+
+            var body = JObject.Parse(tokenResult.Content.ReadAsStringAsync().Result);
+
+            _token = (string)body["access_token"];
+        }
+
+        protected override async Task<HttpResponseMessage> GetAsync(string uri)
+        {
+            return await _server.CreateRequest(uri)
+                .AddHeader("Authorization", "Bearer" + _token)
+                .GetAsync();
+        }
+    }
+}
