e8d41a946f969f35b4109369c5444696be776cd2
Frannsoft <frannsoftdev@outlook.com>
2016-04-16 10:02:12 -0400

    - UserDto has parameterless ctor (bitten by this again with cryptic errors) - Removed more unnecessary properties

--- KuroganeHammer.Data/KuroganeHammer.Data.Api/DTOs/UserDto.cs
+++ KuroganeHammer.Data/KuroganeHammer.Data.Api/DTOs/UserDto.cs
@@ -7,23 +7,27 @@ namespace KuroganeHammer.Data.Api.DTOs
     {
         private readonly UrlHelper _urlHelper;
 
-        public string Url { get; set; }
+        //public string Url { get; set; }
         public string Id { get; set; }
         public string UserName { get; set; }
         public string Email { get; set; }
-        public bool EmailConfirmed { get; set; }
+        //public bool EmailConfirmed { get; set; }
         //public IList<string> Roles { get; set; }
         //public IList<System.Security.Claims.Claim> Claims { get; set; }
 
         public UserDto(ApplicationUser appUser)
         {
-            Url = _urlHelper.Link("GetUser", new { id = appUser.Id });
+            //_urlHelper = new UrlHelper();
+            //Url = _urlHelper.Link("GetUser", new { id = appUser.Id });
             Id = appUser.Id;
             UserName = appUser.UserName;
             Email = appUser.Email;
-            EmailConfirmed = appUser.EmailConfirmed;
+            //EmailConfirmed = appUser.EmailConfirmed;
             //Roles = _appUserManager.GetRolesAsync(appUser.Id).Result;
             //Claims = _appUserManager.GetClaimsAsync(appUser.Id).Result;
         }
+
+        public UserDto()
+        { }
     }
 }
