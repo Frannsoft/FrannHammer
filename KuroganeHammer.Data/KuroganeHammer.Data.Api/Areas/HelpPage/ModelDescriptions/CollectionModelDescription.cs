a9d62dc5feb2a41eaa1aa171c410d8db12c69a1c
Frannsoft <frannsoftdev@outlook.com>
2016-04-17 22:40:35 -0400

    - Removed AspNet Help Page from api project.  No longer using this for api help/docs.

--- KuroganeHammer.Data/KuroganeHammer.Data.Api/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
+++
@@ -1,7 +0,0 @@
-namespace KuroganeHammer.Data.Api.Areas.HelpPage.ModelDescriptions
-{
-    public class CollectionModelDescription : ModelDescription
-    {
-        public ModelDescription ElementDescription { get; set; }
-    }
-}
