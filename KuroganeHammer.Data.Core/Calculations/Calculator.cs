fdfb188533277a88b71dc7301379c1dfbbb08c90
Frannsoft <frannsoftdev@outlook.com>
2016-05-01 22:52:33 -0400

    - Added support in Data.Core.Calculator for VersusModeKnockback and Rage calculation

--- KuroganeHammer.Data.Core/Calculations/Calculator.cs
+++ KuroganeHammer.Data.Core/Calculations/Calculator.cs
@@ -3,12 +3,38 @@
     public class Calculator
     {
         public double TrainingModeKnockback(double victimPercent, int baseDamage, double targetWeight,
-            double knockbackGrowth, int baseKnockbackSetKnockback, double modifier)
+            double knockbackGrowth, int baseKnockbackSetKnockback, double stanceModifier)
         {
             double result = 0;
 
             result = (((victimPercent + baseDamage) / 10 + (victimPercent + baseDamage) * baseDamage / 20) * (200 / (targetWeight + 100)) * 1.4 + 18) * (knockbackGrowth / 100)
-            + baseKnockbackSetKnockback * modifier;
+            + baseKnockbackSetKnockback * stanceModifier;
+
+            return result;
+        }
+
+        public double VersusModeKnockback(double victimPercent, int baseDamage, double staleMoveMultiplier,
+            double targetWeight, double knockbackGrowth, int baseKnockbackSetKnockback, double stanceModifier)
+        {
+            var result = ((((victimPercent + baseDamage * staleMoveMultiplier) / 10 +
+                               (victimPercent + baseDamage * staleMoveMultiplier) * baseDamage *
+                               (1 - (1 - staleMoveMultiplier) * 0.3) / 20)
+                              * 1.4 * (200 / (targetWeight + 100)) + 18) * (knockbackGrowth / 100) + baseKnockbackSetKnockback) *
+                            stanceModifier * Rage(victimPercent);
+
+            return result;
+        }
+
+        public double Rage(double percent)
+        {
+
+            double result = 0;
+
+            if (percent > 35 &&
+                percent < 150)
+            {
+                result = 1 + (percent - 35)*(1.15 - 1)/(150 - 35);
+            }
 
             return result;
         }
--- KuroganeHammer.Data.Core/Tests/CalculateTest.cs
+++ KuroganeHammer.Data.Core/Tests/CalculateTest.cs
@@ -6,13 +6,28 @@ namespace KuroganeHammer.Data.Core.Tests
     [TestFixture]
     public class CalculateTest
     {
+        private Calculator _calculator;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _calculator = new Calculator();
+        }
+
         [Test]
         public void ShouldGetExpectedTrainingModeKnockback()
         {
-            var calculate = new Calculator();
-            var result = calculate.TrainingModeKnockback(100, 20, 80, 10, 31, 1);
+            var result = _calculator.TrainingModeKnockback(100, 20, 80, 10, 31, 1);
 
             Assert.That(result, Is.EqualTo(53.333333333333336d));
+        }
+
+        [Test]
+        public void ShouldGetExpectedVersusModeKnockback()
+        {
+            var result = _calculator.VersusModeKnockback(100, 20, 0, 80, 10, 31, 1);
+
+            Assert.That(result, Is.EqualTo(49.080386473429961d));
         }
 
         [Test]
@@ -20,9 +35,17 @@ namespace KuroganeHammer.Data.Core.Tests
         [TestCase(Modifiers.CrouchCancelling, 0.85)]
         [TestCase(Modifiers.GroundedMeteor, 0.8)]
         [TestCase(Modifiers.Standing, 1)]
-        public void ShouldGetValueAttributeFromModifier_ChargingSmash(Modifiers modifier, double expectedValue)
+        public void ShouldGetValueAttributeFromModifier(Modifiers modifier, double expectedValue)
         {
             Assert.That(modifier.GetModifierValue(), Is.EqualTo(expectedValue));
+        }
+
+        [Test]
+        [TestCase(ElectricModifier.NormalAttack, 1)]
+        [TestCase(ElectricModifier.ElectricAttack, 1.5)]
+        public void ShouldGetValueAttributeFromElectricModifier(ElectricModifier electricModifier, double expectedValue)
+        {
+            Assert.That(electricModifier.GetModifierValue(), Is.EqualTo(expectedValue));
         }
     }
 }
