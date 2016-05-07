90520bac577f192d6e43ee8b6955897fa24aaa08
Frannsoft <frannsoftdev@outlook.com>
2016-05-07 10:16:17 -0400

    - Added support for calculating shieldstun - normal only right now

--- KuroganeHammer.Data/KuroganeHammer.Data.Api/DTOs/CalculatorDtos.cs
+++ KuroganeHammer.Data/KuroganeHammer.Data.Api/DTOs/CalculatorDtos.cs
@@ -2,6 +2,8 @@
 {
     public class CompletedCalculationResponseDto
     {
+        public string CalculatedValueName { get; set;}
+        public string CharacterName { get; set; }
         public string MoveName { get; set; }
         public double CalculatedValue { get; set; }
     }
