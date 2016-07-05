379019fdafcac2de2f55a317b15cfab7ecee428f
Frannsoft <frannsoftdev@outlook.com>
2016-07-04 21:14:08 -0400

    - Dumped unnecessary NotationType enum property

--- FrannHammer.Models/Notation.cs
+++ FrannHammer.Models/Notation.cs
@@ -15,7 +15,6 @@ namespace FrannHammer.Models
     {
         public int Id { get; set; }
         public string Name { get; set; }
-        public NotationTypes NotationType { get; set; }
         public DateTime LastModified { get; set; }
         public override Task<HttpResponseMessage> Create(HttpClient client)
         {
