a1a54717944f23b677f2fa855c8c0b2a2d13a74d
Frannsoft <frannsoftdev@outlook.com>
2016-07-04 21:10:21 -0400

    - Refactored Seed() in configuration file for CFM to work a little more efficiently

--- FrannHammer.Api/Migrations/Configuration.cs
+++ FrannHammer.Api/Migrations/Configuration.cs
@@ -22,6 +22,8 @@ namespace FrannHammer.Api.Migrations
             var adminUserName = ConfigurationManager.AppSettings["adminusername"];
 
             var manager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(new ApplicationDbContext()));
+            var roleManager =
+                new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(new ApplicationDbContext()));
 
             var getUserFound = manager.FindByName(getUserName);
             var adminUserFound = manager.FindByName(adminUserName);
@@ -48,70 +50,98 @@ namespace FrannHammer.Api.Migrations
                 manager.Create(newAdminUser, ConfigurationManager.AppSettings["adminpassword"]);
             }
 
-            //if (!roleManager.Roles.Any())
-            //{
-            //    roleManager.Create(new IdentityRole { Name = "Admin" });
-            //    roleManager.Create(new IdentityRole { Name = "Basic" }); //Just GETs
-            //}
+            if (!roleManager.Roles.Any())
+            {
+                roleManager.Create(new IdentityRole { Name = "Admin" });
+                roleManager.Create(new IdentityRole { Name = "Basic" }); //Just GETs
+            }
 
-            //var adminUser = manager.FindByName("KuroUser");
-            //manager.AddToRoles(adminUser.Id, "Admin");
-            //manager.AddToRoles(adminUser.Id, "Basic");
+            var adminUser = manager.FindByName("KuroUser");
+            manager.AddToRoles(adminUser.Id, "Admin");
+            manager.AddToRoles(adminUser.Id, "Basic");
 
-            //var getUser = manager.FindByName("GETuser");
-            //manager.AddToRoles(getUser.Id, "Basic");
+            var getUser = manager.FindByName("GETuser");
+            manager.AddToRoles(getUser.Id, "Basic");
 
-            ////seed notations
-            //if (!context.Notations.Any())
-            //{
-            //    var floatNotation = new Notation
-            //    {
-            //        Name = "FLOAT",
-            //        NotationType = NotationTypes.Float,
-            //        LastModified = DateTime.Now
-            //    };
+            //seed notations
+            const string notationFloat = "FLOAT";
+            const string notationFrames = "FRAMES";
+            const string notationBool = "BOOLEAN";
 
-            //    var framesNotation = new Notation
-            //    {
-            //        Name = "FRAMES",
-            //        NotationType = NotationTypes.Frames,
-            //        LastModified = DateTime.Now
-            //    };
+            if (!context.Notations.Any())
+            {
+                var floatNotation = new Notation
+                {
+                    Name = notationFloat,
+                    LastModified = DateTime.Now
+                };
 
-            //    var booleanNotation = new Notation
-            //    {
-            //        Name = "BOOLEAN",
-            //        NotationType = NotationTypes.Boolean,
-            //        LastModified = DateTime.Now
-            //    };
+                var framesNotation = new Notation
+                {
+                    Name = notationFrames,
+                    LastModified = DateTime.Now
+                };
 
-            //    context.Notations.Add(floatNotation);
-            //    context.Notations.Add(framesNotation);
-            //    context.Notations.Add(booleanNotation);
-            //}
+                var booleanNotation = new Notation
+                {
+                    Name = notationBool,
+                    LastModified = DateTime.Now
+                };
 
-            //if (context.CharacterAttributes.Any() && !context.CharacterAttributeTypes.Any())
-            //{
-            //    var charAttributeNames = context.CharacterAttributes.Select(c => c.Name).Distinct();//get names
+                context.Notations.Add(floatNotation);
+                context.Notations.Add(framesNotation);
+                context.Notations.Add(booleanNotation);
+            }
 
-            //    //add char attribute type ids
-            //    foreach (var name in charAttributeNames)
-            //    {
-            //        var charAttributeType = new CharacterAttributeType
-            //        {
-            //            Name = name
-            //        };
-            //        context.CharacterAttributeTypes.Add(charAttributeType);
-            //    }
-            //    context.SaveChanges();
+            if (context.CharacterAttributes.Any() && !context.CharacterAttributeTypes.Any())
+            {
+                var charAttributeNames = context.CharacterAttributes.Select(c => c.Name).Distinct().ToList();//get names
 
-            //    //now set the characterattributes
-            //    foreach (var characterAttribute in context.CharacterAttributes.ToList())
-            //    {
-            //        characterAttribute.CharacterAttributeTypeId = context.CharacterAttributeTypes.Single(c => c.Name.Equals(characterAttribute.Name)).Id;
-            //        context.CharacterAttributes.AddOrUpdate(characterAttribute);
-            //    }
-            //}
+                //add char attribute type ids
+                foreach (var name in charAttributeNames)
+                {
+                    int notationId;
+
+                    if (name.Equals("FAST FALL SPEED") ||
+                        name.Equals("MAX AIR SPEED VALUE") ||
+                        name.Equals("MAX FALL SPEED VALUE") ||
+                        name.Equals("MAX FALL SPEED") ||
+                        name.Equals("MAX WALK SPEED VALUE") ||
+                        name.Equals("SPEED INCREASE") ||
+                        name.Equals("VALUE") ||
+                        name.Equals("WEIGHT VALUE"))
+                    {
+                        notationId = context.Notations.First(n => n.Name.Equals(notationFloat)).Id;
+                    }
+                    else if (name.Equals("INTANGIBILITY") ||
+                             name.Equals("INTANGIBLE") ||
+                             name.Equals("FAF"))
+                    {
+                        notationId = context.Notations.First(n => n.Name.Equals(notationFrames)).Id;
+                    }
+                    else
+                    {
+                        notationId = context.Notations.First(n => n.Name.Equals(notationBool)).Id;
+                    }
+
+
+                    var charAttributeType = new CharacterAttributeType
+                    {
+                        NotationId = notationId,
+                        LastModified = DateTime.Now,
+                        Name = name
+                    };
+                    context.CharacterAttributeTypes.Add(charAttributeType);
+                }
+                context.SaveChanges();
+
+                //now set the characterattributes
+                foreach (var characterAttribute in context.CharacterAttributes.ToList())
+                {
+                    characterAttribute.CharacterAttributeTypeId = context.CharacterAttributeTypes.Single(c => c.Name.Equals(characterAttribute.Name)).Id;
+                    context.CharacterAttributes.AddOrUpdate(characterAttribute);
+                }
+            }
 
 
         }
