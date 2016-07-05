31b2cc1d771d8063fefd1efaafe9295b4a9f0525
Frannsoft <frannsoftdev@outlook.com>
2016-07-04 21:16:13 -0400

    - NotationId is no longer nullable

--- FrannHammer.Models/CharacterAttributeType.cs
+++ FrannHammer.Models/CharacterAttributeType.cs
@@ -9,7 +9,7 @@ namespace FrannHammer.Models
         public int Id { get; set; }
         public string Name { get; set; }
         public Notation Notation { get; set; }
-        public int? NotationId { get; set; }
+        public int NotationId { get; set; }
         public DateTime LastModified { get; set; }
 
         public override async Task<HttpResponseMessage> Create(HttpClient client)
