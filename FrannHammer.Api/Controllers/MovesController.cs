5fa563e83910d6840644fba173f0834f62ee6fec
Frannsoft <frannsoftdev@outlook.com>
2016-07-09 21:04:51 -0400

    - Completed issue #88 - Added tests

--- FrannHammer.Api/Controllers/MovesController.cs
+++ FrannHammer.Api/Controllers/MovesController.cs
@@ -17,6 +17,8 @@ namespace FrannHammer.Api.Controllers
     [RoutePrefix("api")]
     public class MovesController : BaseApiController
     {
+        private const string MovesRouteKey = "moves";
+
         public MovesController(ApplicationDbContext context)
             : base(context)
         { }
@@ -27,7 +29,7 @@ namespace FrannHammer.Api.Controllers
         /// </summary>
         /// <returns></returns>
         [ResponseType(typeof(IQueryable<MoveDto>))]
-        [Route("moves")]
+        [Route(MovesRouteKey)]
         public IQueryable<MoveDto> GetMoves()
         {
             return Db.Moves.ProjectTo<MoveDto>();
@@ -39,10 +41,86 @@ namespace FrannHammer.Api.Controllers
         /// <param name="name"></param>
         /// <returns></returns>
         [ResponseType(typeof(IQueryable<MoveDto>))]
-        [Route("moves/byname")]
+        [Route(MovesRouteKey + "/byname")]
         public IQueryable<MoveDto> GetMovesByName([FromUri] string name)
         {
             return Db.Moves.Where(m => m.Name.Equals(name, StringComparison.OrdinalIgnoreCase)).ProjectTo<MoveDto>();
+        }
+
+        /// <summary>
+        /// Get the <see cref="Hitbox"/> data associated with this move.
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        [Route(MovesRouteKey + "/{id}/hitboxes")]
+        public IHttpActionResult GetMoveHitboxData(int id)
+        {
+            var entity = Db.Hitbox.SingleOrDefault(h => h.MoveId == id);
+
+            if (entity == null)
+            {
+                return NotFound();
+            }
+
+            var dto = Mapper.Map<Hitbox, HitboxDto>(entity);
+            return Ok(dto);
+        }
+
+        /// <summary>
+        /// Get the <see cref="Angle"/> data associated with this move.
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        [Route(MovesRouteKey + "/{id}/angles")]
+        public IHttpActionResult GetMoveAngleData(int id)
+        {
+            var entity = Db.Angle.SingleOrDefault(a => a.MoveId == id);
+
+            if (entity == null)
+            {
+                return NotFound();
+            }
+
+            var dto = Mapper.Map<Angle, AngleDto>(entity);
+            return Ok(dto);
+        }
+
+        /// <summary>
+        /// Get the <see cref="BaseDamage"/> data associated with this move.
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        [Route(MovesRouteKey + "/{id}/basedamages")]
+        public IHttpActionResult GetMoveBaseDamageData(int id)
+        {
+            var entity = Db.BaseDamage.SingleOrDefault(b => b.MoveId == id);
+
+            if (entity == null)
+            {
+                return NotFound();
+            }
+
+            var dto = Mapper.Map<BaseDamage, BaseDamageDto>(entity);
+            return Ok(dto);
+        }
+
+        /// <summary>
+        /// Get the <see cref="KnockbackGrowth"/> data associated with this <see cref="Move"/>.
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        [Route(MovesRouteKey + "/{id}/knockbackgrowths")]
+        public IHttpActionResult GetMoveKnockbackGrowthData(int id)
+        {
+            var entity = Db.KnockbackGrowth.SingleOrDefault(b => b.MoveId == id);
+
+            if (entity == null)
+            {
+                return NotFound();
+            }
+
+            var dto = Mapper.Map<KnockbackGrowth, KnockbackGrowthDto>(entity);
+            return Ok(dto);
         }
 
         /// <summary>
@@ -51,7 +129,7 @@ namespace FrannHammer.Api.Controllers
         /// <param name="id"></param>
         /// <returns></returns>
         [ResponseType(typeof(MoveDto))]
-        [Route("moves/{id}")]
+        [Route(MovesRouteKey + "/{id}")]
         public IHttpActionResult GetMove(int id)
         {
             Move move = Db.Moves.Find(id);
@@ -72,7 +150,7 @@ namespace FrannHammer.Api.Controllers
         /// <returns></returns>
         [Authorize(Roles = RolesConstants.Admin)]
         [ResponseType(typeof(IHttpActionResult))]
-        [Route("moves/{id}")]
+        [Route(MovesRouteKey + "/{id}")]
         public IHttpActionResult PutMove(int id, MoveDto dto)
         {
             if (!ModelState.IsValid)
@@ -108,7 +186,7 @@ namespace FrannHammer.Api.Controllers
         /// <returns></returns>
         [Authorize(Roles = RolesConstants.Admin)]
         [ResponseType(typeof(MoveDto))]
-        [Route("moves")]
+        [Route(MovesRouteKey + "")]
         public IHttpActionResult PostMove(MoveDto dto)
         {
             if (!ModelState.IsValid)
@@ -122,7 +200,7 @@ namespace FrannHammer.Api.Controllers
             Db.SaveChanges();
 
             var newDto = Mapper.Map<Move, MoveDto>(entity);
-            return CreatedAtRoute("DefaultApi", new { controller = "Moves", id = newDto.Id }, newDto);
+            return CreatedAtRoute("DefaultApi", new { controller = MovesRouteKey + "", id = newDto.Id }, newDto);
         }
 
         /// <summary>
@@ -132,7 +210,7 @@ namespace FrannHammer.Api.Controllers
         /// <returns></returns>
         [Authorize(Roles = RolesConstants.Admin)]
         [ResponseType(typeof(Move))]
-        [Route("moves/{id}")]
+        [Route(MovesRouteKey + "/{id}")]
         public IHttpActionResult DeleteMove(int id)
         {
             Move move = Db.Moves.Find(id);
