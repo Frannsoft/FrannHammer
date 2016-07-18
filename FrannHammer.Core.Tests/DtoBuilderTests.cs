0527f804e502ddb950a67f89005fdc4a5e19930f
Frannsoft <frannsoftdev@outlook.com>
2016-07-17 20:07:38 -0400

    - Moved IdentityModels - Created ability to dynamically form response based a 'fields' param in all GETs - Completed Issue # 52 - Completed Issue # 37 - Completed Issue # 58

---
+++ FrannHammer.Core.Tests/DtoBuilderTests.cs
@@ -0,0 +1,80 @@
+﻿using FrannHammer.Models;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Reflection;
+
+namespace FrannHammer.Core.Tests
+{
+    [TestFixture]
+    public class DtoBuilderTests
+    {
+        private void AssertObjectBuiltAsExpected(object entity, string rawFields)
+        {
+            var builder = new DtoBuilder();
+
+            string[] fieldsArr = rawFields.Split(',');
+            string splitFields = string.Join(",", fieldsArr);
+
+            var result = builder.Build(entity, splitFields);
+
+            var exValues = result as IDictionary<string, object>;
+
+            Debug.Assert(exValues != null, "exValues != null");
+
+            CollectionAssert.AllItemsAreNotNull(exValues);
+            CollectionAssert.AllItemsAreUnique(exValues);
+            CollectionAssert.IsNotEmpty(exValues);
+
+            var entityValuesThatExist = (from value in entity.GetType().GetProperties(BindingFlags.Instance | BindingFlags.Public)
+                                         join name in fieldsArr
+                                             on value.Name.ToLower() equals name
+                                         select value).ToList();
+
+            var entityValuesThatDoNotExistOnEo = (from value in entity.GetType().GetProperties(BindingFlags.Instance | BindingFlags.Public)
+                                                  where !fieldsArr.Any(f => f.Equals(value.Name.ToLower()))
+                                                  select value).ToList();
+
+            CollectionAssert.AllItemsAreNotNull(entityValuesThatExist);
+            CollectionAssert.AllItemsAreUnique(entityValuesThatExist);
+            CollectionAssert.IsNotEmpty(entityValuesThatExist);
+            Assert.That(entityValuesThatExist.Count, Is.EqualTo(fieldsArr.Length));
+
+            //assert that the specified field values are present and equal to the entity object's values
+            foreach (var val in entityValuesThatExist)
+            {
+                Assert.That(exValues[val.Name], Is.EqualTo(val.GetValue(entity)));
+            }
+
+            //assert that all fields not specified in the fields array are not present on the dynamic object
+            foreach (var val in entityValuesThatDoNotExistOnEo)
+            {
+                Assert.That(!exValues.ContainsKey(val.Name));
+            }
+        }
+
+        [Test]
+        [TestCase("name")]
+        [TestCase("lastmodified,name")]
+        [TestCase("id,style,lastmodified,colortheme")]
+        public void ShouldBuildObjectWithSpecificProps(string rawFields)
+        {
+            var entity = new Character
+            {
+                Id = 1,
+                ColorTheme = null,
+                Description = "desc",
+                DisplayName = string.Empty,
+                LastModified = DateTime.Now,
+                MainImageUrl = "url",
+                Name = "test",
+                Style = "thestyle",
+                ThumbnailUrl = "thumburl"
+            };
+
+            AssertObjectBuiltAsExpected(entity, rawFields);
+        }
+    }
+}
---
+++ FrannHammer.Core/DtoBuilder.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Reflection;
+
+namespace FrannHammer.Core
+{
+    /// <summary>
+    /// Supports building dynamic objects via <see cref="ExpandoObject"/> from an
+    /// object and specified property values from that object.
+    /// 
+    /// A poor man's version of Facebook's graph api feature.
+    /// </summary>
+    public class DtoBuilder
+    {
+        private const BindingFlags Flags = BindingFlags.Instance | BindingFlags.Public | BindingFlags.IgnoreCase;
+
+        /// <summary>
+        /// Build up an <see cref="ExpandoObject"/> consisting of the specified field values
+        /// as they exist on the passed in <typeparamref name="TEntity"/>.
+        /// </summary>
+        /// <typeparam name="TEntity"></typeparam>
+        /// <param name="entity"></param>
+        /// <param name="fieldsRaw"></param>
+        /// <returns></returns>
+        public dynamic Build<TEntity>(TEntity entity, string fieldsRaw)
+        {
+            Guard.VerifyStringIsNotNullOrEmpty(fieldsRaw, nameof(fieldsRaw));
+            Guard.VerifyObjectNotNull(entity, nameof(entity));
+
+            var splitValues = SplitValues(fieldsRaw);
+
+            return Assemble(entity, splitValues);
+        }
+
+        private IEnumerable<string> SplitValues(string fieldsRaw)
+        {
+            return fieldsRaw.Split(',');
+        }
+
+        private dynamic Assemble<TEntity>(TEntity entity, IEnumerable<string> requestedFieldNames)
+        {
+            Guard.VerifyObjectNotNull(requestedFieldNames, nameof(requestedFieldNames));
+
+            var fieldsNamesList = requestedFieldNames.ToList();
+            if (!fieldsNamesList.Any())
+            { throw new ArgumentException("Must have at least one field specified! "); }
+
+            var customDto = new Dictionary<string, object>();
+
+            foreach (var field in fieldsNamesList)
+            {
+                var propInfo = entity.GetType().GetProperty(field, Flags);
+
+                if (propInfo != null)
+                {
+                    var value = entity.GetType()
+                        .GetProperty(field, Flags);
+
+                    customDto.Add(propInfo.Name, value.GetValue(entity));
+                }
+            }
+
+            dynamic resultObj = customDto.ToDynamicObject();
+            return resultObj;
+        }
+    }
+}
