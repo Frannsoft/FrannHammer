f4e36191d35e63a01d0376238bd97ea1a3b9661d
Frannsoft <frannsoftdev@outlook.com>
2016-07-21 21:55:37 -0400

    - Added comments - Added Base/Set Knockback model objects to AutoMapper config in startup - Project file modifications

--- FrannHammer.Api/Properties/AssemblyInfo.cs
+++ FrannHammer.Api/Properties/AssemblyInfo.cs
@@ -14,6 +14,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: InternalsVisibleTo("FrannHammer.Api.Tests")]
+[assembly: InternalsVisibleTo("FrannHammer.Services.Tests")]
 
 // Setting ComVisible to false makes the types in this assembly not visible
 // to COM components.  If you need to access a type in this assembly from
