b418df1e8d419797cc9c06c0b4c835b5d561e6b5
Frannsoft <frannsoftdev@outlook.com>
2016-10-17 21:49:04 -0400

    Added overload for IsBetween
    This is to support comparisons that will never have more than one value (e.g., FirstActionableFrame)

--- FrannHammer.Services/RangeQuantifierService.cs
+++ FrannHammer.Services/RangeQuantifierService.cs
@@ -2,8 +2,32 @@
 {
     public class RangeQuantifierService
     {
-        public bool IsBetween(int valueUnderTest, int lowBoundary, int highBoundary) =>
-             valueUnderTest > lowBoundary && valueUnderTest < highBoundary;
+        public bool IsBetween(int valueUnderTest, int lowBoundary, int highBoundary)
+        {
+            if (highBoundary > -1)
+            {
+                return valueUnderTest > lowBoundary && valueUnderTest < highBoundary;
+            }
+            else
+            {
+                return valueUnderTest > lowBoundary;
+            }
+        }
+
+        public bool IsBetween(int lowValueUnderTest, int lowBoundary, int highValueUnderTest, int highBoundary)
+        {
+            if (highBoundary > -1)
+            {
+                return (lowValueUnderTest > lowBoundary && highValueUnderTest < highBoundary) ||
+                       (lowValueUnderTest < lowBoundary && highValueUnderTest > lowBoundary);
+            }
+            else
+            {
+                return lowValueUnderTest > lowBoundary && 
+                       highValueUnderTest > lowBoundary && 
+                       highValueUnderTest >= lowValueUnderTest;
+            }
+        }
 
         public bool IsGreaterThan(int valueUnderTest, int boundary) =>
             valueUnderTest > boundary;
