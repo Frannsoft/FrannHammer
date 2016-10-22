a8620ac9b6f96ed004445d84c468872a43d5e372
Frannsoft <frannsoftdev@outlook.com>
2016-10-21 22:07:37 -0400

    Minor refactoring
    - Delete test classes that were no longer necessary
    - Spruced up SearchPredicateFactory and implemented remaining properties off of ComplexMoveSearchModel.

--- FrannHammer.Services/SearchPredicateFactory.cs
+++ FrannHammer.Services/SearchPredicateFactory.cs
@@ -5,32 +5,39 @@ namespace FrannHammer.Services
 {
     public class SearchPredicateFactory
     {
-        private readonly HitboxStartupSearchPredicateService _hitboxSearchPredicateService;
-        private readonly BaseDamageSearchPredicateService _baseDamagePredicateService;
+        //these have custom range checks
         private readonly HitboxActiveSearchPredicateService _hitboxActiveSearchPredicateService;
         private readonly NameSearchPredicateService _nameSearchExpressionService;
         private readonly FirstActionableFrameSearchPredicateService _firstActionableFrameSearchPredicateService;
 
-        private readonly SearchPredicateService _searchPredicateService;
+        //uses default range checks
+        private readonly SearchPredicateService _baseMoveHitboxPredicateService;
 
         public SearchPredicateFactory()
         {
-            _hitboxSearchPredicateService = new HitboxStartupSearchPredicateService();
-            _baseDamagePredicateService = new BaseDamageSearchPredicateService();
             _nameSearchExpressionService = new NameSearchPredicateService();
             _hitboxActiveSearchPredicateService = new HitboxActiveSearchPredicateService();
             _firstActionableFrameSearchPredicateService = new FirstActionableFrameSearchPredicateService();
-            _searchPredicateService = new SearchPredicateService();
+            _baseMoveHitboxPredicateService = new SearchPredicateService();
         }
 
         public Func<Hitbox, bool> CreateHitboxStartupPredicate(ComplexMoveSearchModel searchModel)
-            => _searchPredicateService.GetPredicate<Hitbox>(searchModel.HitboxStartupFrame);
+            => _baseMoveHitboxPredicateService.GetPredicate<Hitbox>(searchModel.HitboxStartupFrame);
 
         public Func<BaseDamage, bool> CreateBaseDamagePredicate(ComplexMoveSearchModel searchModel)
-            => _searchPredicateService.GetPredicate<BaseDamage>(searchModel.BaseDamage);
+            => _baseMoveHitboxPredicateService.GetPredicate<BaseDamage>(searchModel.BaseDamage);
 
         public Func<Angle, bool> CreateAnglePredicate(ComplexMoveSearchModel searchModel)
-            => _searchPredicateService.GetPredicate<Angle>(searchModel.Angle);
+            => _baseMoveHitboxPredicateService.GetPredicate<Angle>(searchModel.Angle);
+
+        public Func<BaseKnockback, bool> CreateBaseKnockbackPredicate(ComplexMoveSearchModel searchModel)
+            => _baseMoveHitboxPredicateService.GetPredicate<BaseKnockback>(searchModel.BaseKnockback);
+
+        public Func<SetKnockback, bool> CreateSetKnockbackPredicate(ComplexMoveSearchModel searchModel)
+           => _baseMoveHitboxPredicateService.GetPredicate<SetKnockback>(searchModel.SetKnockback);
+
+        public Func<KnockbackGrowth, bool> CreateKnockbackGrowthPredicate(ComplexMoveSearchModel searchModel)
+           => _baseMoveHitboxPredicateService.GetPredicate<KnockbackGrowth>(searchModel.KnockbackGrowth);
 
         public Func<Hitbox, bool> CreateHitboxActiveOnFramePredicate(ComplexMoveSearchModel searchModel)
             => _hitboxActiveSearchPredicateService.GetHitboxActivePredicate(searchModel.HitboxActiveOnFrame);
