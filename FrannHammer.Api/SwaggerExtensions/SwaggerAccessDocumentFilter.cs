4b24640cab4e61d436556f8e5d86196e5a7e6ea3
Frannsoft <frannsoftdev@outlook.com>
2017-02-16 22:17:17 -0500

    - Modified styling of swagger page - Added more info to swagger page - Restricted available calls to GETs on swagger page

---
+++ FrannHammer.Api/SwaggerExtensions/SwaggerAccessDocumentFilter.cs
@@ -0,0 +1,22 @@
+﻿using System.Web.Http.Description;
+using Swashbuckle.Swagger;
+
+namespace FrannHammer.Api.SwaggerExtensions
+{
+    public class SwaggerAccessDocumentFilter : IDocumentFilter
+    {
+        public void Apply(SwaggerDocument swaggerDoc, SchemaRegistry schemaRegistry, IApiExplorer apiExplorer)
+        {
+            foreach (var pathItem in swaggerDoc.paths.Values)
+            {
+                pathItem.delete = null;
+                //pathItem.get = NullifyIfRestricted(pathItem.get);
+                pathItem.head = null;
+                pathItem.options = null;
+                pathItem.patch = null;
+                pathItem.post = null;
+                pathItem.put = null;
+            }
+        }
+    }
+}
