ce52c2932251e36dfed86edbc7c1097d283be319
FrannDotExe <frannsoftdev@outlook.com>
2017-06-03 22:08:57 -0400

    - Cleaned up unused move service code in character controller.

--- FrannHammer.WebApi/Controllers/CharacterController.cs
+++ FrannHammer.WebApi/Controllers/CharacterController.cs
@@ -20,15 +20,11 @@ namespace FrannHammer.WebApi.Controllers
         private const string DetailedMovesRouteKey = "/detailedmoves";
         private const string SearchRouteKey = "/search";
         private readonly ICharacterService _characterService;
-        private readonly IMoveService _moveService;
 
-        public CharacterController(ICharacterService characterService, IMoveService moveService)
+        public CharacterController(ICharacterService characterService)
         {
             Guard.VerifyObjectNotNull(characterService, nameof(characterService));
-            Guard.VerifyObjectNotNull(moveService, nameof(moveService));
-
             _characterService = characterService;
-            _moveService = moveService;
         }
 
         [Route(CharactersRouteKey + IdRouteKey)]
@@ -55,7 +51,6 @@ namespace FrannHammer.WebApi.Controllers
         [Route(CharactersRouteKey + NameRouteKey + ThrowsRouteKey)]
         public IHttpActionResult GetAllThrowsForCharacterWhereName(string name, [FromUri]string fields = "")
         {
-            //var throws = _moveService.GetAllThrowsWhereCharacterNameIs(name, fields);
             var throws = _characterService.GetAllThrowsWhereCharacterNameIs(name, fields);
             return Result(throws);
         }
@@ -63,7 +58,6 @@ namespace FrannHammer.WebApi.Controllers
         [Route(CharactersRouteKey + IdRouteKey + ThrowsRouteKey)]
         public IHttpActionResult GetAllThrowsForCharacterWhereOwnerId(int id, [FromUri] string fields = "")
         {
-            //var throws = _moveService.GetAllThrowsWhereCharacterOwnerIdIs(id, fields);
             var throws = _characterService.GetAllThrowsWhereCharacterOwnerIdIs(id, fields);
             return Result(throws);
         }
@@ -71,7 +65,6 @@ namespace FrannHammer.WebApi.Controllers
         [Route(CharactersRouteKey + NameRouteKey + MovesRouteKey + SearchRouteKey)]
         public IHttpActionResult GetAllMovesForCharacterByNameWhereFilter([FromUri] MoveFilterResourceQuery query)
         {
-            //var moves = _moveService.GetAllWhere(query);
             var moves = _characterService.GetAllMovesForCharacterByOwnerIdWhereFilter(query);
             return Result(moves);
         }
@@ -79,7 +72,6 @@ namespace FrannHammer.WebApi.Controllers
         [Route(CharactersRouteKey + IdRouteKey + MovesRouteKey + SearchRouteKey)]
         public IHttpActionResult GetAllMovesForCharacterByIdWhereFilter([FromUri] MoveFilterResourceQuery query)
         {
-            //var moves = _moveService.GetAllWhere(query);
             var moves = _characterService.GetAllMovesForCharacterByNameWhereFilter(query);
             return Result(moves);
         }
@@ -87,7 +79,6 @@ namespace FrannHammer.WebApi.Controllers
         [Route(CharactersRouteKey + NameRouteKey + MovesRouteKey)]
         public IHttpActionResult GetAllMovesForCharacterWhereName(string name, [FromUri] string fields = "")
         {
-            //var moves = _moveService.GetAllThrowsWhereCharacterNameIs(name, fields);
             var moves = _characterService.GetAllMovesWhereCharacterNameIs(name, fields);
             return Result(moves);
         }
@@ -95,7 +86,6 @@ namespace FrannHammer.WebApi.Controllers
         [Route(CharactersRouteKey + IdRouteKey + MovesRouteKey)]
         public IHttpActionResult GetAllMovesForCharacterWhereOwnerId(int id, [FromUri] string fields = "")
         {
-            //var moves = _moveService.GetAllThrowsWhereCharacterOwnerIdIs(id, fields);
             var moves = _characterService.GetAllMovesWhereCharacterOwnerIdIs(id, fields);
             return Result(moves);
         }
