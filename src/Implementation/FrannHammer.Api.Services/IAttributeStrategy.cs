74d77822d0d49bc51bc772e28204b3076f842611
Frannsoft <Frannsoftdev@outlook.com>
2018-07-01 22:39:24 -0400

    - updated all mongo db driver nuget packages to accomodate impending mlab sandbox instance upgrade (https://docs.mongodb.com/ecosystem/drivers/csharp/#compatibility)

--- src/Implementation/FrannHammer.Api.Services/IAttributeStrategy.cs
+++ src/Implementation/FrannHammer.Api.Services/IAttributeStrategy.cs
@@ -3,8 +3,18 @@ using System.Reflection;
 
 namespace FrannHammer.Api.Services
 {
+    /// <summary>
+    /// The strategy for attributes
+    /// </summary>
     public interface IAttributeStrategy
     {
+        /// <summary>
+        /// Get the attribute from property info.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="instance"></param>
+        /// <param name="propertyInfo"></param>
+        /// <returns></returns>
         T GetAttributeFromProperty<T>(object instance, PropertyInfo propertyInfo)
             where T : Attribute;
     }
