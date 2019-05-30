eb0fc5953b69ebf8ac34950e1d25ef7089f9f0f4
Frannsoft <Frannsoftdev@outlook.com>
2019-05-30 18:33:26 -0400

    - can now compile...

--- src/Implementation/FrannHammer.NetCore.WebApi/Program.cs
+++ src/Implementation/FrannHammer.NetCore.WebApi/Program.cs
@@ -14,7 +14,7 @@ namespace FrannHammer.NetCore.WebApi
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
 #if !DEBUG
-                .UseSentry(Environment.GetEnvironmentVariable("SENTRY_URL")),
+                .UseSentry(Environment.GetEnvironmentVariable("SENTRY_URL"))
 #endif
                 .UseStartup<Startup>();
     }
