3666987114943644d04336575cab286a91e53d99
Frannsoft <Frannsoftdev@outlook.com>
2019-09-11 07:09:56 -0400

    - fixed piranha plant thumbnail url issue - #294

--- src/Implementation/FrannHammer.WebScraping.Domain/PiranhaPlant.cs
+++ src/Implementation/FrannHammer.WebScraping.Domain/PiranhaPlant.cs
@@ -5,7 +5,7 @@ namespace FrannHammer.WebScraping.Domain
     public class PiranhaPlant : WebCharacter
     {
         public PiranhaPlant()
-            : base("PiranhaPlant", "Piranha%20Plant")
+            : base("PiranhaPlant", "Piranha%20Plant", potentialScrapingNames: "Piranha Plant")
         {
             CssKey = "potplant";
             DisplayName = "Pirahna Plant";
