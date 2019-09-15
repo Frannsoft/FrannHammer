fba70eb9f25eaaa090283f525e0bbe8aa0585884
Frannsoft <Frannsoftdev@outlook.com>
2019-09-15 08:17:09 -0400

    - added support for ice climbers

---
+++ src/Implementation/FrannHammer.WebScraping.Domain/IceClimbers.cs
@@ -0,0 +1,14 @@
+﻿using FrannHammer.WebScraping.Domain.Contracts;
+
+namespace FrannHammer.WebScraping.Domain
+{
+    public class IceClimbers : WebCharacter
+    {
+        public IceClimbers()
+            : base("IceClimbers", "Ice-Climbers", potentialScrapingNames: "Ice Climbers")
+        {
+            CssKey = "nana";
+            DisplayName = "Ice Climbers";
+        }
+    }
+}
