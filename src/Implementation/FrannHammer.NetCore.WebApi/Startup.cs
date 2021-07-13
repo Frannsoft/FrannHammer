5c8ff51f6e57ff6ad02c3d0cd034ecfe34995a0b
Frannsoft <Frannsoftdev@outlook.com>
2021-07-13 10:40:10 -0400

    fixing seed data

--- src/Implementation/FrannHammer.NetCore.WebApi/Startup.cs
+++ src/Implementation/FrannHammer.NetCore.WebApi/Startup.cs
@@ -146,7 +146,7 @@ namespace FrannHammer.NetCore.WebApi
 
         private void SeedData(ICharacterDataScraper characterDataScraper, DebugStorageLocationResolver fileLocationResolver)
         {
-#if DEBUG
+#if !DEBUG
             var charactersToSeed = Characters.All;
             foreach (var character in charactersToSeed)
             {
@@ -177,7 +177,7 @@ namespace FrannHammer.NetCore.WebApi
                 }
             }
 #endif
-#if !DEBUG
+#if DEBUG
             _characterData.AddRange(JsonConvert.DeserializeObject<List<Character>>(File.ReadAllText(fileLocationResolver.Character)));
             _moveData.AddRange(JsonConvert.DeserializeObject<List<Move>>(File.ReadAllText(fileLocationResolver.Move)));
             _movementData.AddRange(JsonConvert.DeserializeObject<List<Movement>>(File.ReadAllText(fileLocationResolver.Movement)));
